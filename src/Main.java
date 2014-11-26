0bb66e992157ee0f37e32c5ff80c120a46db6c79
kingdorian <doriandekoning@hotmail.com>
2014-11-26 09:22:05 +0100

    Improved parser
    - Renamed and rewrote readNEWGAME to readCompetition
    - Wrote method parseTeam to parse a team
    - Wrote method parsePlayer to parse a player

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args){
 		try{
-			XMLHandler.readXML("XML/competition.xml");
+			XMLHandler.readNEWGAME("XML/competition.xml");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
--- src/XMLHandler.java
+++ src/XMLHandler.java
@@ -13,22 +13,43 @@ import org.w3c.dom.NodeList;
 public class XMLHandler {
 		static final String Encoding = "UTF-8";
 		
-		public static void readNEWGAME(String filename) throws Exception{
-			ArrayList teamNames = new ArrayList<String>();
+		public static ArrayList<Team> readCompetition(String filename) throws Exception{
+			ArrayList teams = new ArrayList<String>();
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = db.parse(new File(filename));
 			Element rootEl = doc.getDocumentElement();
-			NodeList nodes = rootEl.getChildNodes();
+			NodeList teamnodes = rootEl.getChildNodes();
+			for(int i = 0; i<teamnodes.getLength();i++){
+				teams.add(parseTeam((Element)teamnodes.item(i)));
+			}
+			return teams;
+			
+		}
+		public static Team parseTeam(Element teamElement){
+			ArrayList<Players> players = new ArrayList<Players>();
+			NodeList nodes = teamElement.getChildNodes();
 			for(int i =0; i<nodes.getLength();i++){
 				Node node = nodes.item(i);
-				if(node instanceof Element){
-					Element el = (Element)node;
-					String name = el.getChildNodes().item(1).getTextContent();
-					String grassType = el.getChildNodes().item(3).getTextContent();
-					String urlToImg = el.getChildNodes().item(5).getTextContent();
+				Element el = (Element)node;
+				players.add(parsePlayer(el));
+			}
+			return new Team(null, null, 0, null, 0, 0, 0, 0);
+		}
+		
+		public static Player parsePlayer(Element playerElement){
+			String name;
+			NodeList nl = playerElement.getChildNodes();
+			for(int j = 0; j<nl.getLength();j++){
+				switch(nl.item(j).getNodeName()){
+				//Add cases here to parse certain elements
+					case "Name":
+						name = nl.item(j).getTextContent();
+						break;
 				}
 			}
+			return null;
+			//return new Player(0, name, name, 0, 0, 0, 0, 0, 0, 0);
 		}
-		public static void storeGAME()
+
 }
