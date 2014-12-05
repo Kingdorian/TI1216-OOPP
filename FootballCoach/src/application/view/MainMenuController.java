f3579990e5a20aa594e5db0e883dd5e4ceff9b0c
Faris Elghlan <faris.elghlan@hotmail.com>
2014-12-06 00:52:02 +0100

    Added functionality to the Settings and Exit buttons (exit button should be further optimized as explained in the comments and Settings is for testing and as an example of how to use the createPopup() method)

--- FootballCoach/src/application/view/MainMenuController.java
+++ FootballCoach/src/application/view/MainMenuController.java
@@ -11,6 +11,7 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
+import org.controlsfx.dialog.Dialogs;
 
 /**
  *
@@ -53,10 +54,41 @@ public class MainMenuController implements ViewControllerInterface {
      * Method to switch to the game's HOME screen.
      */
     @FXML
-    private void button(){
+    private void buttonContinue(){
         mainController.setCenterView("GameScreenHOME");
         mainController.setLeftView("GameScreenMenu");
         mainController.setTopView("GameScreenTitle");
     }
+    
+    
+    /**
+     * Method to close the application
+     */
+    @FXML
+    private void buttonExit(){
+        // show pop-up: are you sure you want to quit? if ok is clicked: quit game, else don't quit game
+        System.exit(0);
+    }
    
+    
+    /**
+     * current content only for testing !
+     */
+    @FXML
+    private void buttonSettings(){
+        //TEST
+        if(mainController.createPopup("TEST_POP_UP_TEST", "<<<<<<<<<<<<<<<<<<< notice the picture ||| (title) this is a test", "/application/img/Settings.png"))
+            Dialogs.create()
+                .title("pop-up closing status")
+                .masthead("The pop-up has closed succesfully!")
+                .message("The OK button was used to close the pop-up screen!\nCongratulations, now you can update the values entered in the pop-up screen!")
+                .showInformation();
+        else
+            Dialogs.create()
+                .title("pop-up closing status")
+                .masthead("The pop-up was killed!")
+                .message("Unfortunately the OK button has NOT been pushed :(\nAt least there weren't any changes commited, so you won't have to do anything!")
+                .showWarning();
+        //TEST
+    }
 }
