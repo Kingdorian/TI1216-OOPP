a89a42271f32ac5b621f946a5895a3d935e9d5cf
Faris Elghlan <faris.elghlan@hotmail.com>
2014-12-06 00:54:59 +0100

    A new interface for pop-up screens which should be implemented by all pop-up screens (= secondary screens)

---
+++ FootballCoach/src/application/view/PopupControllerInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application.view;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author faris
+ */
+public interface PopupControllerInterface{
+    public boolean isOkClicked();
+    public void setPopupStage(Stage popupStage);
+}
