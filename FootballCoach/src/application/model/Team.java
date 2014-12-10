e3224eaae181cd5e4fd085f3ceedb315234103e1
jochem725 <Jochem@wlan-145-94-144-148.wlan.tudelft.nl>
2014-12-10 15:18:53 +0100

    Ege: Transfers

--- FootballCoach/src/application/model/Team.java
+++ FootballCoach/src/application/model/Team.java
@@ -24,7 +24,6 @@ public class Team {
 		this.goalsAgainst = 0;
 		this.artificialGrass = artificalGrass;
 	}
-
 	public boolean hasArtificialGrass() {
 		return artificialGrass;
 	}
@@ -125,6 +124,20 @@ public class Team {
 	 */
 	public void addPlayer(Players p){
 		players.add(p);
+	}
+	/**
+	 * Makes a transfer from the team to the other team t
+	 * @param p Player which is changing team
+	 * @param t Team which te player goes to
+	 * @param money The transfersum
+	 */
+	public void transferTo(Players p, Team t, int money){
+		if(this.getPlayers().contains(p)){
+			this.getPlayers().remove(p);
+			t.addPlayer(p);
+			this.setBudget(this.getBudget() + money);
+			t.setBudget(t.getBudget() - money);
+		}
 	}
 
 
--- FootballCoach/src/application/modelTest/TeamTest.java
+++ FootballCoach/src/application/modelTest/TeamTest.java
@@ -2,11 +2,8 @@ package application.modelTest;
 
 import application.model.*;
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.junit.Test;
-
 
 public class TeamTest {
 
@@ -27,6 +24,41 @@ public class TeamTest {
 		assertEquals(team1, team2);
 		assertNotEquals(team1, team3);
 		
+	}
+	
+	@Test
+	public void testTransferTo() {
+		Team team1 = new Team("Ajax", false);
+		Team team2 = new Team("Ado", false);
+		Team team3 = new Team("AZ", false);
+		Team team4 = new Team("Excelsior", false);
+		
+		Player player1 = new Player("Bob", "De Bouwer", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+		Player player2 = new Player("Ken", "De Bouweer", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+		Player player3 = new Player("Paul", "De Bouker", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+		
+		team1.setBudget(50000);
+		team2.setBudget(100000);
+		team3.setBudget(100000);
+		team4.setBudget(50000);
+		
+		team1.addPlayer(player1);
+		team1.addPlayer(player2);
+		team1.addPlayer(player3);
+		team2.addPlayer(player1);
+		team2.addPlayer(player2);
+		team3.addPlayer(player1);
+		team3.addPlayer(player2);
+		team4.addPlayer(player1);
+		team4.addPlayer(player2);
+		team4.addPlayer(player3);
+		
+		team1.transferTo(player3, team2, 50000);
+		
+		assertEquals(team1.getBudget(), team3.getBudget());
+		assertEquals(team1.getPlayers(), team3.getPlayers());
+		assertEquals(team2.getBudget(), team4.getBudget());
+		assertEquals(team2.getPlayers(), team4.getPlayers());
 	}
 
 }
