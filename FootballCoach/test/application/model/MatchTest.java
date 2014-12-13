bd55782076d161100bbf0a69ab1cb95f78136d70
Faris Elghlan <faris.elghlan@hotmail.com>
2014-12-13 14:16:53 +0100

    Moved test files to own folder &  package

---
+++ FootballCoach/test/application/model/MatchTest.java
@@ -0,0 +1,120 @@
+package application.model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author faris
+ */
+public class MatchTest {
+
+    Match match;
+    Team homeTeam;
+    Team outTeam;
+    ArrayList<Players> homeList;
+    ArrayList<Players> visitorList;
+
+    @Before
+    public void intialize() {
+        homeTeam = new Team("home", 100000, 0, 0, 0, false);
+        outTeam = new Team("out", 100000, 0, 0, 0, false);
+
+        homeList = new ArrayList<>();
+        homeList.add(new Goalkeeper("name", "sname", 0, Status.DEFAULT, 0, Reason.DEFAULT, 100, 100));
+        for (int i = 1; i < 11; i++) {
+            homeList.add(new Player("name", "sname", i, Status.DEFAULT, 0, Reason.DEFAULT, 100, 100, 100));
+        }
+        homeTeam.setPlayers(homeList);
+
+        visitorList = new ArrayList<>();
+        visitorList.add(new Goalkeeper("name", "sname", 0, Status.DEFAULT, 0, Reason.DEFAULT, 100, 100));
+        for (int i = 1; i < 11; i++) {
+            visitorList.add(new Player("name", "sname", i, Status.DEFAULT, 0, Reason.DEFAULT, 100, 100, 100));
+        }
+        outTeam.setPlayers(visitorList);
+
+        match = new Match(homeTeam, outTeam, 2, 3);
+    }
+
+    public MatchTest() {
+    }
+
+    /**
+     * Test of getHomeTeam method, of class Match.
+     */
+    @Test
+    public void testGetHomeTeam() {
+        assertTrue(homeTeam.equals(match.getHomeTeam()));
+    }
+
+    /**
+     * Test of setHomeTeam method, of class Match.
+     */
+    @Test
+    public void testSetHomeTeam() {
+        Match newMatch = new Match(null, null, 0, 0);
+        newMatch.setHomeTeam(homeTeam);
+        assertTrue(newMatch.getHomeTeam().equals(homeTeam));
+    }
+
+    /**
+     * Test of getVisitorTeam method, of class Match.
+     */
+    @Test
+    public void testGetVisitorTeam() {
+        assertTrue(outTeam.equals(match.getVisitorTeam()));
+    }
+
+    /**
+     * Test of setVisitorTeam method, of class Match.
+     */
+    @Test
+    public void testSetVisitorTeam() {
+        Match newMatch = new Match(null, null, 0, 0);
+        newMatch.setVisitorTeam(outTeam);
+        assertTrue(newMatch.getVisitorTeam().equals(outTeam));
+    }
+
+    /**
+     * Test of getPointsHomeTeam method, of class Match.
+     */
+    @Test
+    public void testGetPointsHomeTeam() {
+        assertTrue(match.getPointsHomeTeam() == 2);
+    }
+
+    /**
+     * Test of setPointsHomeTeam method, of class Match.
+     */
+    @Test
+    public void testSetPointsHomeTeam() {
+        match.setPointsHomeTeam(5);
+        assertTrue(match.getPointsHomeTeam() == 5);
+    }
+
+    /**
+     * Test of getPointsVisitorTeam method, of class Match.
+     */
+    @Test
+    public void testGetPointsVisitorTeam() {
+        assertTrue(match.getPointsVisitorTeam() == 3);
+    }
+
+    /**
+     * Test of setPointsVisitorTeam method, of class Match.
+     */
+    @Test
+    public void testSetPointsVisitorTeam() {
+        match.setPointsVisitorTeam(5);
+        assertTrue(match.getPointsVisitorTeam() == 5);
+    }
+
+}
---
+++ FootballCoach/test/application/model/PlayerTest.java
@@ -0,0 +1,20 @@
+package application.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void testPlayer() {
+
+        Player player1 = new Player("Bob", "De Bouwer", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+        Player player2 = new Player("Bob", "De Bouwer", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+        Player player3 = new Player("Bob", "De Bouwer", 9, Status.DEFAULT, 5, Reason.DEFAULT, 90, 60, 80);
+
+        assertEquals(player1, player2);
+        assertFalse(player1.equals(player3));
+    }
+
+}
