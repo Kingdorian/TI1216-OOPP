f380fb42d87b19c8ad6293dc3e97a8d86b0b2308
Faris Elghlan <faris.elghlan@hotmail.com>
2014-12-28 18:29:32 +0100

    Made a start with select player (for Ege). Run the TEST_Select_Player instead of the Main file to run the player selection part (it is not yet added to the main application).

---
+++ FootballCoach/src/application/TEST_Select_Player.java
@@ -0,0 +1,149 @@
+package application;
+
+
+import application.animation.Playmatch.ChoosePositionsController;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.transform.Scale;
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author faris
+ */
+public class TEST_Select_Player extends Application {
+    
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    @Override
+    public void start(Stage stage) {
+        primaryStage = stage;
+        stage.setResizable(false);
+        initializeRootLayout();
+    }
+
+    
+    public void initializeRootLayout(){
+        try {
+            // Load root layout from fxml file
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(TEST_Select_Player.class.getResource("animation/Playmatch/ChoosePositions.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+            
+            //set pane to controller
+            ChoosePositionsController.drawCircles(rootLayout);
+            
+            // Show the scene containing the root layout
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(true);
+
+            primaryStage.show();
+            
+            scaleToScreenSize(rootLayout);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Failed to load interface: AnchorPane");
+        }
+    }
+    
+    /**
+     *
+     * @param args Command Line arguments.
+     */
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    
+    // This code to adjust the screen size is coppied from http://stackoverflow.com/a/16608161
+    // A few minor adjustments have been made to improve it and comments have been added.
+    //****************************************************************************************************
+    /**
+     * Scale the size of the given stage. coppied from: see header above
+     *
+     * @param contentPane the stage to resize
+     */
+    private void scaleToScreenSize(final AnchorPane contentPane) {
+        Scene scene = contentPane.getScene();
+        final double initWidth = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio = initWidth / initHeight;
+
+        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
+
+    /**
+     * When the user resizes the screen the method changed() in this class will
+     * be called coppied from: see header above
+     */
+    private static class SceneSizeChangeListener implements ChangeListener<Number> {
+
+        private final Scene scene;
+        private final double ratio;
+        private final double initHeight;
+        private final double initWidth;
+        private final AnchorPane contentPane;
+
+        public SceneSizeChangeListener(Scene scene, double ratio, double initHeight, double initWidth, AnchorPane contentPane) {
+            this.scene = scene;
+            this.ratio = ratio;
+            this.initHeight = initHeight;
+            this.initWidth = initWidth;
+            this.contentPane = contentPane;
+        }
+
+        /**
+         * Listen for size changes and change the scene accordingly coppied
+         * from: see header above
+         *
+         * @param observableValue the value being observed
+         * @param oldValue old size
+         * @param newValue new size
+         */
+        @Override
+        public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+            final double newWidth = scene.getWidth();
+            final double newHeight = scene.getHeight();
+
+            double scaleFactor
+                    = newWidth / newHeight > ratio
+                            ? newHeight / initHeight
+                            : newWidth / initWidth;
+
+            //if (scaleFactor >= 1) {
+                Scale scale = new Scale(scaleFactor, scaleFactor);
+                scale.setPivotX(0);
+                scale.setPivotY(0);
+                scene.getRoot().getTransforms().setAll(scale);
+
+                contentPane.setPrefWidth(newWidth / scaleFactor);
+                contentPane.setPrefHeight(newHeight / scaleFactor);
+//            } else {
+//                contentPane.setPrefWidth(Math.max(initWidth, newWidth));
+//                contentPane.setPrefHeight(Math.max(initHeight, newHeight));
+//            }
+        }
+    }
+    //****************************************************************************************************
+    // End of coppied code.
+    
+}
--- FootballCoach/src/application/animation/Playmatch/ChoosePositionsController.java
+++ FootballCoach/src/application/animation/Playmatch/ChoosePositionsController.java
@@ -5,10 +5,19 @@
  */
 package application.animation.Playmatch;
 
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.effect.Lighting;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,29 +25,176 @@ import javafx.scene.shape.Circle;
  * @author faris
  */
 public class ChoosePositionsController {
+private static final Circle playerCircle[] = new Circle[10];
 
-    @FXML
-    private Circle circle;
+    /**
+     * this class implements EventHandler and can 
+     * be added to a circle to make it dragable when
+     * the cursor moves over the circle (because we
+     * can't add the event handlers directly when 
+     * loading, since the circles won't be referrenced
+     * by the fxml file yet)
+     */
+    private static class CircleDragHandler implements EventHandler{
+        private final Circle c;
+        
+        /**
+         * Constructor
+         * @param c Circle to add event handler to
+         */
+        public CircleDragHandler(final Circle c){
+            this.c = c;
+        }
+        
+        /**
+         * Change the x and y coordinate of the circle when the circle is dragged
+         * and change cursors look when mouse events occur
+         * @param event and event (nothing will happen if this isn't a mouse event)
+         */
+        @Override
+        public void handle(Event event){
+            //make the circle dragable when any mouse event occurs
+            c.getScene().getRoot().setCursor(Cursor.HAND);
+            makeDragable(c);
+            
+            //remove this event handler, so it will only get triggered once
+            c.removeEventHandler(MouseEvent.ANY, this);
+        }
+    }
+    
     
     /**
      * Initializes the controller class.
      */
     @FXML
     private void initialize() {
-        
-        circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
-        @Override 
-        public void handle(MouseEvent event) {
-            circle.setCenterX(event.getX());
-            circle.setCenterY(event.getY());
-        }
-      });
     }    
     
-    @FXML
-    private void draggingCircle(){
-        //circle.setCenterX(Cursor.);
+
+    /**
+     * This method makes the group of the circle dragable.
+     * This method is based on the makeDragable method from 
+     * this project:
+     * https://gist.github.com/jewelsea/3388637
+     *
+     * @param circle  the circle who's group should be made dragable
+     */
+    public static void makeDragable(Circle circle){
+
+        // class to store relative x and y coordinates
+        class Delta {
+            double x, y;
+        }
         
+        final Delta dragDelta = new Delta();
+        Node node = circle.getScene().getRoot();
+        
+        // when clicking set the position of the group relative to the mouse
+        // and change the mouse cursor to 'move'
+        circle.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                // record a delta distance for the drag and drop operation.
+                dragDelta.x = circle.getParent().getLayoutX() - mouseEvent.getScreenX();
+                dragDelta.y = circle.getParent().getLayoutY() - mouseEvent.getScreenY();
+                node.setCursor(Cursor.MOVE);
+                
+                // reset all circles colors
+                for(int i=0; i<10; i++)
+                    playerCircle[i].setFill(Color.BLUE);
+                
+                // set this circles color to a different color, so it seems
+                // as if it is selected
+                circle.setFill(Color.AQUA);
+            }
+        });
+        
+        // when releasing the mouse button, change the cursor to a hand
+        circle.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                node.setCursor(Cursor.HAND);
+            }
+        });
+        
+        // move the group when dragging with the mouse (relative to where the mouse is)
+        circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                circle.getParent().setLayoutX(mouseEvent.getScreenX() + dragDelta.x);
+                circle.getParent().setLayoutY(mouseEvent.getScreenY() + dragDelta.y);
+            }
+        });
+        
+        // change cursor to a hand when entering the group
+        circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (!mouseEvent.isPrimaryButtonDown()) {
+                    node.setCursor(Cursor.HAND);
+                }
+            }
+        });
+        
+        // change to default cursor when leaving the group
+        circle.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (!mouseEvent.isPrimaryButtonDown()) {
+                    node.setCursor(Cursor.DEFAULT);
+                }
+            }
+        });
     }
     
+    
+    /**
+     * Add the player circles to the scene
+     * @param anchorPane the anchor pane containing the scene
+     */
+    public static void drawCircles(AnchorPane anchorPane){
+        
+        // create the circles
+        playerCircle[0] = new Circle(330, 160, 13, Color.BLUE);
+        playerCircle[1] = new Circle(288, 290, 13, Color.BLUE);
+        playerCircle[2] = new Circle(288, 467, 13, Color.BLUE);
+        playerCircle[3] = new Circle(330, 605, 13, Color.BLUE);
+        playerCircle[4] = new Circle(550, 250, 13, Color.BLUE);
+        playerCircle[5] = new Circle(450, 385, 13, Color.BLUE);
+        playerCircle[6] = new Circle(550, 513, 13, Color.BLUE);
+        playerCircle[7] = new Circle(713, 259, 13, Color.BLUE);
+        playerCircle[8] = new Circle(785, 385, 13, Color.BLUE);
+        playerCircle[9] = new Circle(719, 494, 13, Color.BLUE);
+
+        // add players name to the circles + set a lighting effect
+        // and add the circle+text to the scene
+        for(int i=0; i<10; i++){
+            // add player name
+            Text text = new Text();
+            text.setText("Player " + i);
+            text.setX(playerCircle[i].getCenterX() - 25);
+            text.setY(playerCircle[i].getCenterY() - 18);
+            text.setFill(Color.WHITE);
+            text.setFont(new Font("Verdana bold", 14));
+            
+            // add lighting effect
+            playerCircle[i].setEffect(new Lighting());
+            
+            // group circle and name togheter and add them to the scene
+            Group group = new Group(playerCircle[i], text);
+            anchorPane.getChildren().add(group);
+        }
+
+        // make all circles dragable (indirectly via an event handler)
+        playerCircle[0].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[0]));
+        playerCircle[1].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[1]));
+        playerCircle[2].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[2]));
+        playerCircle[3].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[3]));
+        playerCircle[4].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[4]));
+        playerCircle[5].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[5]));
+        playerCircle[6].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[6]));
+        playerCircle[7].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[7]));
+        playerCircle[8].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[8]));
+        playerCircle[9].addEventHandler(MouseEvent.ANY, new CircleDragHandler(playerCircle[9]));
+    }
 }
